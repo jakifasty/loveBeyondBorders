3e316381cae28d7e52ed6bb08839c0e061cbfa3c
Borja Javierre <borja.general@gmail.com>
2023-01-26 00:18:43 +0100

    initial commit with initial files

---
+++ love-borders-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Love Beyond Borders
+        </p>
+      </header>
+    </div>
+
+  );
+
+  /*<div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>*/
+}
+
+export default App;
